e5f06630a70d6d9a8303f69b8a4baa9302c2dc61
Ashin Mathew Thomas <ashinm1@umbc.edu>
2021-09-15 19:31:19 -0400

    animates buttons and make sounds on click

--- Simon/game.js
+++ Simon/game.js
@@ -4,9 +4,29 @@ let correctPattern = [];
 let userClickedPattern = [];
 let gameStart = false;
 
+console.log("hello"); // making sure the javascript file actually runs
 
+// making sure the user pressed any key from keybpard to stard game
+document.addEventListener("keydown", event => {
+  // start game if its not already started
+  if(!gameStart) {
+    gameStart = true;
+    console.log("Game starts");
+  }
+});
 
+const onClick = function() {
+//  console.log(this.id);
+  let userChoice = this.id  // set the button id to useChoice
+  sound(userChoice);  // call sound function to play sound based on color
+}
+document.getElementById('red').onclick = onClick;
+document.getElementById('blue').onclick = onClick;
+document.getElementById('green').onclick = onClick;
+document.getElementById('yellow').onclick = onClick;
 
-function nextColor(){
-
+// function that takes button color and plays the sound based on the color
+function sound(buttonColor){
+  let audio = new Audio("sounds/" + buttonColor + ".mp3");
+  audio.play();
 }
