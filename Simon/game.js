30916f2bd4033c07910ec15b7b23d502d0400653
Ashin Mathew Thomas <ashinm1@umbc.edu>
2021-09-15 14:11:56 -0400

    testing branch commit

---
+++ Simon/game.js
@@ -0,0 +1,12 @@
+const buttonColor = ["red", "blue", "green", "yellow"];
+let level = 0;
+let correctPattern = [];
+let userClickedPattern = [];
+let gameStart = false;
+
+
+
+
+function nextColor(){
+
+}
